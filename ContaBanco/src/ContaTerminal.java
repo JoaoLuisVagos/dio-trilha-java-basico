dd3d251debc8b12d70cad6f58dd13bba3b3030ce
João Luis Vagos <joaoluisvagos@gmail.com>
2025-08-06 12:31:34 -0300

    feat: Adicionado .java e scanner para projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TODO: Conhecer e importar a classe scanner
+        Scanner scanner = new Scanner(System.in); 
+
+        // Exibir a mensagem para o nosso usuario
+        System.out.println("Qual o seu nome?");
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Qual o número da sua agência?");
+        String agencia = scanner.nextLine();
+        System.out.println("Qual o número da sua conta?");
+        Integer numero = scanner.nextInt();
+        System.out.println("Qual o seu saldo?");
+        Double saldo = scanner.nextDouble();
+
+        // Imprimir as informações
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
